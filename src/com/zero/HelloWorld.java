b6b732ba19ebc7b5a4f3b573bd28eaad401a2593
ruei wang <14790379688@163.com>
2019-07-15 21:25:33 +0800

    提交到本地

---
+++ src/com/zero/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zero;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("你好！");
+         System.out.println("Helloween，爱理不理");
+	}
+
+}
